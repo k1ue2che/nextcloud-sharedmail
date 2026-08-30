ac7d886ba8077184ed20e0fd8f3e026e40cffba5
k1ue2che <k1@hunix.info>
2026-08-30 14:13:52 +0200

    Fix frontend route access for Nextcloud 34

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -6,6 +6,8 @@ namespace OCA\SharedMail\Controller;
 
 use OCA\SharedMail\AppInfo\Application;
 use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\Attribute\NoAdminRequired;
+use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\IRequest;
 
@@ -17,6 +19,8 @@ class PageController extends Controller
         parent::__construct(Application::APP_ID, $request);
     }
 
+    #[NoCSRFRequired]
+    #[NoAdminRequired]
     public function index(): TemplateResponse
     {
         return new TemplateResponse(
